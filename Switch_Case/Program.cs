c1dcafea5bb82f536996bb8ca0850d50cba1e3af
ozlem55 <65675763+ozlem55@users.noreply.github.com>
2022-02-19 14:53:14 +0300

    Initial commit

---
+++ Switch_Case/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasnız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasnız");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasnız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasnız");
+                    break;
+
+
+                default:
+                    Console.WriteLine("Yanlış veri girişi");
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış ayındasınız");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar ayındasınız");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz ayındasınız");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar ayındasınız");
+                    break;
+
+
+
+                default:
+                    break;
+            }
+        }
+    }
+}
